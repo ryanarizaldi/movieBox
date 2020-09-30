ce288e5d45f1c618f5ce632d59737ae78ede68d5
Ryan Ahmad Rizaldi <ryana@thebigrichgroup.com>
2020-09-30 16:07:52 +0800

    test commit

---
+++ src/pages/Detail.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import DetailMovie from "../components/DetailMovie";
+
+export default class Detail extends Component {
+  render() {
+    return (
+      <div>
+        <DetailMovie />
+      </div>
+    );
+  }
+}
