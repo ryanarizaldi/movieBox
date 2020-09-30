a041ce11b40a7be065e1320be2b6a570443481a0
Ryan Ahmad Rizaldi <ryana@thebigrichgroup.com>
2020-10-01 02:12:42 +0800

    try adding login functionality. cant resolve email blank error from mas amir API

--- src/components/DetailMovie.jsx
+++ src/components/DetailMovie.jsx
@@ -1,7 +1,90 @@
 import React, { Component } from "react";
+import { Container, Image, Button, Badge, Row, Col } from "react-bootstrap";
 
 export default class DetailMovie extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <Image src="" fluid />
+        <Container>
+          <h1>Judul</h1>
+          <h4>Score</h4>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam sint
+            accusamus soluta quaerat ut doloribus itaque vitae, odio beatae, sit
+            aliquid porro aspernatur magni ratione amet nesciunt. Repellat,
+            earum libero! Fugit voluptates repellat omnis eveniet, repellendus
+            sed deserunt dicta, praesentium culpa qui accusantium voluptatum?
+            Quisquam vel vero minus delectus distinctio necessitatibus
+            perspiciatis
+          </p>
+          <Button>Watch trailer</Button>
+          <Button>Add to Watchlist</Button>
+
+          <div className="movie-badge">
+            {/* <Button> */}
+            <Badge pill variant="info">
+              Overview
+            </Badge>
+            {/* </Button> */}
+            <Badge pill variant="info">
+              Character
+            </Badge>
+            <Badge pill variant="info">
+              Review
+            </Badge>
+          </div>
+
+          <div className="overview">
+            <h1>Synopsis</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi
+              alias in qui eos earum fugit maiores esse ullam nemo rerum eius
+              sint architecto, reiciendis quae explicabo consequatur, et
+              distinctio sed dignissimos corrupti saepe aut id molestiae ipsum.
+              Quibusdam, quo rem libero asperiores non eum, quasi ipsa aperiam
+              hic ut sit? Lorem ipsum dolor sit amet consectetur adipisicing
+              elit. Molestias expedita voluptatum quis odit laudantium.
+              Similique ut quas porro nam commodi eum sit, inventore cumque
+              architecto neque voluptate incidunt odit dolorem, tenetur veniam
+              enim nulla quibusdam asperiores voluptatum atque. Possimus dolores
+              rerum saepe officia earum iure vel esse expedita aspernatur
+              laudantium.
+            </p>
+            <Row>
+              <Col lg="2">
+                <b>Release Date</b>
+              </Col>
+              <Col lg="10">: January 25, 1342</Col>
+              <Col lg="2">
+                <b>Director</b>
+              </Col>
+              <Col lg="10">: Michael Bay</Col>
+              <Col lg="2">
+                <b>Other detail</b>
+              </Col>
+              <Col lg="10">
+                : Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Tempora, optio.
+              </Col>
+              <Col lg="2">
+                <b>Other detail</b>
+              </Col>
+              <Col lg="10">
+                : Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Tempora, optio.
+              </Col>
+              <Col lg="2">
+                <b>Other detail</b>
+              </Col>
+              <Col lg="10">
+                : Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Tempora, optio.
+              </Col>
+            </Row>
+          </div>
+        </Container>
+      </div>
+    );
   }
 }
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -7,44 +7,186 @@ import {
   FormControl,
   Button,
   InputGroup,
+  Modal,
 } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import axios from "axios";
 
 export default class Navigation extends Component {
+  state = {
+    showLogin: false,
+    showSign: false,
+    emailLogin: "",
+    passLogin: "",
+    token: localStorage.getItem("login"),
+  };
+
+  onChange = (name, e) => {
+    this.setState({
+      [name]: e,
+    });
+    // console.log(this.state);
+  };
+
+  login = async (e) => {
+    e.preventDefault();
+    try {
+      const { emailLogin, passLogin } = this.state;
+      console.log(emailLogin, passLogin);
+      const submit = await axios.post(
+        "http://appdoto.herokuapp.com/api/users/login",
+        {
+          emailLogin,
+          passLogin,
+        }
+      );
+
+      localStorage.setItem("login", submit.data.data.token);
+      this.props.onChange("login", submit.data.data.token);
+      this.props.onClose();
+
+      console.log("submit", submit);
+    } catch (error) {
+      console.log("error", error);
+    }
+  };
+
+  launchModalLogin = () => {
+    this.setState({ showLogin: true });
+  };
+
+  launchModalSign = () => {
+    this.setState({ showSign: true });
+  };
+
+  handleCloseLogin = () => {
+    this.setState({ showLogin: false });
+  };
+
+  handleCloseSign = () => {
+    this.setState({ showSign: false });
+  };
+
   render() {
+    const { emailLogin, passLogin } = this.state;
     return (
-      <Navbar expand="lg" className="navbar">
-        <Container>
-          <Navbar.Brand>
-            <Link to="/">
-              <FontAwesomeIcon icon="video" />
-              MOVIEBOX
-            </Link>
-          </Navbar.Brand>
-          <InputGroup className="w-50">
-            <FormControl
-              placeholder="Search Movie..."
-              aria-label="Search Movie..."
-              aria-describedby="basic-addon2"
-            />
-            <InputGroup.Append>
-              <Button variant="outline-secondary">Search</Button>
-            </InputGroup.Append>
-          </InputGroup>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ml-auto">
-              <Link to="/" className="nav-link">
-                Sign Up
+      <>
+        <Navbar expand="lg" className="navbar">
+          <Container>
+            <Navbar.Brand>
+              <Link to="/">
+                <FontAwesomeIcon icon="video" />
+                MOVIEBOX
               </Link>
-              <Link to="/about" className="nav-link">
-                Login
-              </Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+            </Navbar.Brand>
+            <InputGroup className="w-50">
+              <FormControl
+                placeholder="Search Movie..."
+                aria-label="Search Movie..."
+                aria-describedby="basic-addon2"
+              />
+              <InputGroup.Append>
+                <Button variant="outline-secondary">Search</Button>
+              </InputGroup.Append>
+            </InputGroup>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ml-auto">
+                <Button onClick={this.launchModalSign} variant="light">
+                  Sign Up
+                </Button>
+                <Button onClick={this.launchModalLogin} variant="light">
+                  Login
+                </Button>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <Modal
+          size="lg"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+          show={this.state.showSign}
+          onHide={this.handleCloseSign}
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">
+              Sign Up
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label>Full Name</Form.Label>
+                <Form.Control
+                  type="fullName"
+                  placeholder="Enter your Fullname"
+                />
+              </Form.Group>
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+              </Form.Group>
+
+              <Form.Group controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+              </Form.Group>
+
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.handleCloseSign}>Close</Button>
+          </Modal.Footer>
+        </Modal>
+        <Modal
+          size="lg"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+          show={this.state.showLogin}
+          onHide={this.handleCloseLogin}
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">Login</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form onSubmit={this.login}>
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter email"
+                  name="emailLogin"
+                  value={emailLogin}
+                  onChange={(e) => this.onChange(e.target.name, e.target.value)}
+                />
+              </Form.Group>
+
+              <Form.Group controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  value={passLogin}
+                  type="password"
+                  placeholder="Password"
+                  name="passLogin"
+                  onChange={(e) => this.onChange(e.target.name, e.target.value)}
+                />
+              </Form.Group>
+
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.handleCloseLogin}>Close</Button>
+          </Modal.Footer>
+        </Modal>
+      </>
     );
   }
 }
