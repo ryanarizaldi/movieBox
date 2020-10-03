d9f449c0a20e5d76db8b67bbd016d319d802ca54
Ryan Ahmad Rizaldi <ryana@thebigrichgroup.com>
2020-10-03 11:56:14 +0800

    adding sort by genre button in home

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,34 +1,10 @@
 import React, { Component } from "react";
-import {
-  Carousel,
-  Container,
-  Image,
-  Button,
-  Row,
-  Col,
-  Card,
-  Pagination,
-} from "react-bootstrap";
+import { Carousel } from "react-bootstrap";
 import dp from "../assets/img/hero1.jpg";
 import dp1 from "../assets/img/hero.jpg";
 import dp2 from "../assets/img/hero1.jpg";
+import Categories from "./categories/MainCategory";
 export default class Hero extends Component {
-  state = {
-    movies: [],
-  };
-
-  componentDidMount = () => {
-    fetch(
-      "https://api.themoviedb.org/3/movie/popular?api_key=0f4cb6189e20110c05e4b524ae7821ac"
-    )
-      .then((response) => response.json())
-      .then((json) => {
-        this.setState({
-          movies: json.results,
-        });
-      });
-  };
-
   render() {
     return (
       <>
@@ -43,61 +19,7 @@ export default class Hero extends Component {
             <img className="d-block w-100 h-50" src={dp2} alt="Third slide" />
           </Carousel.Item>
         </Carousel>
-        <Container>
-          <div className="show-by">
-            <h1>Show by Genre</h1>
-            <Button active>Sci-fi</Button>
-            <Button>Drama</Button>
-            <Button>Comedy</Button>
-            <Button>Horor</Button>
-            <Button>Romance</Button>
-            <Button>Action</Button>
-            <Button>Anime</Button>
-          </div>
-          <div className="movie-by-genre">
-            <Row>
-              {this.state.movies ? (
-                this.state.movies.slice(0, 20).map((mov) => (
-                  <Col md="3">
-                    <Card>
-                      <Card.Img
-                        variant="top" 
-                        src={"http://image.tmdb.org/t/p/w500" + mov.poster_path}
-                      />
-                      <Card.Body>
-                        <Card.Title>{mov.original_title}</Card.Title>
-                        <Card.Text>{mov.release_date.slice(0, 4)}</Card.Text>
-                      </Card.Body>
-                    </Card>
-                  </Col>
-                ))
-              ) : (
-                <Col>
-                  <Card>
-                    <Card.Img variant="top" src="../assets/img/404.jpg" />
-                    <Card.Body>
-                      <Card.Title>No Movie Found</Card.Title>
-                    </Card.Body>
-                  </Card>
-                </Col>
-              )}
-            </Row>
-          </div>
-          <Row id="page">
-            <Pagination>
-              <Pagination.First />
-              <Pagination.Prev />
-              <Pagination.Item active>{1}</Pagination.Item>
-              <Pagination.Item>{2}</Pagination.Item>
-              <Pagination.Item>{3}</Pagination.Item>
-              <Pagination.Ellipsis />
-              <Pagination.Item>{14}</Pagination.Item>
-              <Pagination.Ellipsis />
-              <Pagination.Next />
-              <Pagination.Last />
-            </Pagination>
-          </Row>
-        </Container>
+        <Categories />
       </>
     );
   }
---
+++ src/components/categories/MainCategory.jsx
@@ -0,0 +1,159 @@
+import React, { Component } from "react";
+import { Container, Button, Row, Col, Card, Pagination } from "react-bootstrap";
+import axios from "axios";
+
+export default class MainCategory extends Component {
+  state = {
+    movies: [],
+    option: 0,
+    active: 0,
+    genre: [],
+    currPage: 1,
+    totRes: 0,
+  };
+
+  componentDidMount = () => {
+    this.getMovie();
+    this.getGenre();
+  };
+
+  getMovie = () => {
+    const { option } = this.state;
+    if (option === 0) {
+      console.log("masuk option");
+      fetch(
+        "https://api.themoviedb.org/3/movie/popular?api_key=0f4cb6189e20110c05e4b524ae7821ac"
+      )
+        .then((response) => response.json())
+        .then((json) => {
+          this.setState({
+            movies: json.results,
+            totRes: json.total_pages,
+          });
+        });
+    } else {
+      console.log("masuk option custom");
+      fetch(
+        `https://api.themoviedb.org/3/discover/movie?api_key=0f4cb6189e20110c05e4b524ae7821ac&with_genres=${option}`
+      )
+        .then((response) => response.json())
+        .then((json) => {
+          this.setState({
+            movies: json.results,
+            totRes: json.total_pages,
+          });
+        });
+    }
+  };
+
+  //INI TETEP DITULIS PREVSTATENYA MESKIPUN GA DIPAKE
+  componentDidUpdate = (prevProps, prevState) => {
+    const { option } = this.state;
+    if (option !== prevState.option) {
+      this.getMovie();
+    }
+  };
+
+  genre = (num) => {
+    this.setState({
+      option: num,
+      active: num,
+    });
+  };
+
+  getGenre = () => {
+    fetch(
+      `https://api.themoviedb.org/3/genre/movie/list?api_key=0f4cb6189e20110c05e4b524ae7821ac`
+    )
+      .then((response) => response.json())
+      .then((json) => {
+        this.setState({
+          genre: json.genres,
+        });
+      });
+  };
+
+  nextPage = async (pageNum) => {
+    try {
+      const { option } = this.state;
+      const data = await axios.get(
+        `https://api.themoviedb.org/3/discover/movie?api_key=0f4cb6189e20110c05e4b524ae7821ac&with_genres=${option}&page=${pageNum}`
+      );
+      this.setState({
+        movies: data.data.data,
+      });
+    } catch (error) {
+      console.log("error: ", error);
+    }
+  };
+
+  render() {
+    const { active, totRes, currPage } = this.state;
+    return (
+      <Container>
+        <div className="show-by">
+          <h1>Show by Genre</h1>
+          <Button
+            className={active === 0 ? `active` : ""}
+            onClick={() => this.genre(0)}
+          >
+            Popular
+          </Button>
+          {this.state.genre.slice(0, 10).map((gen) => (
+            <Button
+              className={active === gen.id ? `active` : ""}
+              onClick={() => this.genre(gen.id)}
+            >
+              {gen.name}
+            </Button>
+          ))}
+        </div>
+        <div className="movie-by-genre">
+          <Row>
+            {this.state.movies ? (
+              this.state.movies.slice(0, 20).map((mov) => (
+                <Col md="3">
+                  <Card>
+                    <Card.Img
+                      variant="top"
+                      src={"http://image.tmdb.org/t/p/w500" + mov.poster_path}
+                    />
+                    <Card.Body>
+                      <Card.Title>{mov.original_title}</Card.Title>
+                      <Card.Text>{mov.release_date.slice(0, 4)}</Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              ))
+            ) : (
+              <Col>
+                <Card>
+                  <Card.Img variant="top" src="../assets/img/404.jpg" />
+                  <Card.Body>
+                    <Card.Title>No Movie Found</Card.Title>
+                  </Card.Body>
+                </Card>
+              </Col>
+            )}
+          </Row>
+        </div>
+        {totRes && (
+          <Row id="page">
+            <Pagination>
+              <Pagination.First />
+              <Pagination.Prev />
+              {currPage && <Pagination.Item active>{currPage}</Pagination.Item>}
+              {/* <Pagination.Item active>{1}</Pagination.Item>
+              <Pagination.Item>{2}</Pagination.Item>
+              <Pagination.Item>{3}</Pagination.Item>
+              <Pagination.Ellipsis />
+              <Pagination.Item>{14}</Pagination.Item> */}
+              <Pagination.Next />
+              <Pagination.Last />
+            </Pagination>
+          </Row>
+        )}
+      </Container>
+    );
+  }
+}
